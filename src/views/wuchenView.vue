ea02f9bd7da6ea4d1e34bd16956429b0ba33a19d
TheStylite <848099532@qq.com>
2022-03-23 13:53:55 +0800

    boolSwitch组件开发完成

--- src/views/wuchenView.vue
+++ src/views/wuchenView.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="title">
     无尘开发的组件
+    <BooleanSwitch v-model="flag"></BooleanSwitch>
   </div>
 </template>
+<script>
+import BooleanSwitch from '@/components/wuchen-components/booleanSwitch.vue';
+
+export default {
+  components: {
+    BooleanSwitch,
+  },
+  data() {
+    return {
+      flag: false,
+    };
+  },
+};
+</script>
