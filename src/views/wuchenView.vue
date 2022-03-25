f09a32a0c27622f79ac1616dcec9598ee1c0d78c
TheStylite <848099532@qq.com>
2022-03-25 13:56:22 +0800

    k8sPhase组件 over

--- src/views/wuchenView.vue
+++ src/views/wuchenView.vue
@@ -4,16 +4,20 @@
     <BooleanSwitch v-model="flag"></BooleanSwitch>
     <br />
     <CheckBox v-model="flag"></CheckBox>
+    <br />
+    <K8sPhase></K8sPhase>
   </div>
 </template>
 <script>
 import BooleanSwitch from '@/components/wuchen-components/booleanSwitch.vue';
 import CheckBox from '@/components/wuchen-components/checkbox.vue';
+import K8sPhase from '@/components/wuchen-components/k8sPhase.vue';
 
 export default {
   components: {
     CheckBox,
     BooleanSwitch,
+    K8sPhase,
   },
   data() {
     return {
